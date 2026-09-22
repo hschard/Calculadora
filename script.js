070d5a909018fbd182ef2b9858325bd3a38353b2
hschard <hschardong0@gmail.com>
2026-09-21 22:17:01 -0300

    comment

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-let v1 = 0;
-let v2 = 0;
+//Input do resultado
 let output = document.querySelector(".inputOut");
+//Botao igual
 const btnEqual = document.querySelector(".result");
 //Valores dos numeros
 const btnKeys = document.querySelectorAll(".insert");
 
 //SOMA
-function sum(value1 = v1, value2 = v2) {
+function sum(value1, value2) {
   console.log(value1 + value2);
 }
 
 //SUBTRAÇÃO
-function sub(value1 = v1, value2 = v2) {
+function sub(value1, value2) {
   console.log(value1 - value2);
 }
 
 //MULTIPLICAÇÃO
-function multi(value1 = v1, value2 = v2) {
+function multi(value1, value2) {
   console.log(value1 * value2);
 }
 
 //DIVISÃO
-function div(value1 = v1, value2 = v2) {
+function div(value1, value2) {
   console.log(value1 / value2);
 }
 
